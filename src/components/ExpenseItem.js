9117b343dd47d93fd1aa484c90229e67daf28078
Akeeme <akeeme@akeemes-mbp-2.myfiosgateway.com>
2021-06-25 13:49:52 -0400

    expense date and styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './ExpenseItem.css';
+import './ExpenseDate';
+import ExpenseDate from './ExpenseDate';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+
     return (
         <div className = "expense-item">
-            <div>Date</div>
+            <ExpenseDate date = { props.date }/>
             <div className = "expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className = "expense-item__price">$248.57</div>
+                <h2>{ props.title }</h2>
+                <div className = "expense-item__price">${ props.amount }</div>
             </div>
         </div>
     )
